45a7edab78d304d5d7637b8a463c3601a11fca25
Goysa2 <samuel.goyette@usherbrooke.ca>
2019-03-21 14:53:09 -0400

    Basic unitary line search

---
+++ src/one-step-size.jl
@@ -0,0 +1,16 @@
+export one_step_size
+
+"""
+Returns 1.0 step size
+"""
+function one_step_size(h           :: LineModel,
+                       stop_ls     :: LS_Stopping,
+                       f_meta      :: LS_Function_Meta;
+                       φ_dφ        :: Function = (x, y) -> phi_dphi(x, y),
+                       verboseLS   :: Bool = false,
+                       kwargs...)
+    state = stop_ls.current_state
+    update!(state, x = 1.0)
+
+    return (state, true)
+end # function
